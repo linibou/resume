c1574add6971bed26524b47e2e4a3a51a1147295
Julien Antony <julien.antony@scopart.fr>
2015-09-12 12:21:20 +0200

    [add] add some effects to header

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-$(document).foundation();
+var resume = resume || {};
+
+(function($, document){
+    resume.main = {
+        Init: function(){
+            $(document).foundation();
+            this.Anim();
+        },
+        Anim: function(){
+            var tl = new TimelineLite();
+            var tl2 = new TimelineLite();
+            tl.from($('#avatar'), 0.8, {scaleX:0, scaleY:0, ease: Back.easeOut});
+            tl.from($('#devop'), 0.8, {css:{bottom:"300px"}, ease: Bounce.easeOut});
+            tl2.from($('#name'), 0.8, {css:{left:"500px", display: "none"}, ease: Back.easeOut});
+            tl2.from($('#age'), 0.3, {css:{top:"100px", display: "none"}});
+            tl2.from($('#phone'), 0.3, {css:{top:"100px", display: "none"}});
+            tl2.from($('#mail'), 0.3, {css:{top:"100px", display: "none"}});
+        },
+    };
+})(jQuery, document);
